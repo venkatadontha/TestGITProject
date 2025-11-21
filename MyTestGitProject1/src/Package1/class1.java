5505cf7260adeb62e733a3d99ca58db1bc565aff
venkatadontha <rajakishore.qa@gmail.com>
2025-11-21 16:46:51 +0000

    Update class1.java
    Checking Git Origin to Eclipse

--- MyTestGitProject1/src/Package1/class1.java
+++ MyTestGitProject1/src/Package1/class1.java
@@ -4,4 +4,6 @@ public class class1 {
 	
 	//new line added to check only the Commit and not push 
 
+	//this is to check that the changes done in git are taken into eclipse by fetching from origin
+
 }
