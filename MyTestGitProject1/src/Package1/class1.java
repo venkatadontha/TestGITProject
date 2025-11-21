e154ddd18f674b1b218bae8a825e86086316ef49
venkatadontha <rajakishore.qa@gmail.com>
2025-11-21 16:47:45 +0000

    Update class1.java
    test1

--- MyTestGitProject1/src/Package1/class1.java
+++ MyTestGitProject1/src/Package1/class1.java
@@ -4,6 +4,6 @@ public class class1 {
 	
 	//new line added to check only the Commit and not push 
 
-	//this is to check that the changes done in git are taken into eclipse by fetching from origin
+	//this is to check that the changes done in git are taken into eclipse by fetching from origin1
 
 }
